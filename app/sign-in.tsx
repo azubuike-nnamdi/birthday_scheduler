d259427f44b638854753852043a7715e48eddc88
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-12 23:33:38 +0100

    refactor: sign in page :zap:

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,23 @@
-import { View, Text, Button, TouchableOpacity } from "react-native";
-import { useGoogleSignIn } from "@/config/signin";
+import { DASHBOARD_URL, HOME_URL } from "@/config/routes";
+import { useAuth } from "@/context/AuthContext";
+import { Redirect } from "expo-router";
+import { View, Text, TouchableOpacity } from "react-native";
 
 function SignIn() {
-  const { userInfo, signIn } = useGoogleSignIn();
+  const { user, signIn } = useAuth();
+
+  if (user) {
+    return <Redirect href={HOME_URL} />;
+  }
 
   return (
-    <View>
-      <TouchableOpacity onPress={signIn} className="bg-blue-500 p-2 rounded-md text-white">
-        <Text className="text-center text-white">Sign In</Text>
+    <View className="flex-1 justify-center items-center p-4">
+      <Text className="text-2xl font-bold mb-8">Welcome</Text>
+      <TouchableOpacity
+        onPress={signIn}
+        className="bg-blue-500 p-4 rounded-md w-full"
+      >
+        <Text className="text-center text-white font-semibold">Sign In with Google</Text>
       </TouchableOpacity>
     </View>
   );
