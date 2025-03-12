29400e2573e17637ed1a95616e32b61ef555c88f
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-12 14:31:33 +0100

    feat: sign-in configuration :racehorse:

---
+++ config/signin.ts
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import {
+  GoogleSignin,
+  isErrorWithCode,
+  isSuccessResponse,
+  statusCodes,
+  type User
+} from '@react-native-google-signin/google-signin';
+
+export const useGoogleSignIn = () => {
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        setUserInfo(response.data);
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
+  return { userInfo, signIn };
+};
