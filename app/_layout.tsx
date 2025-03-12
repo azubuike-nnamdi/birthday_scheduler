4f71d656253db221fae9995710f729257305930b
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-12 23:33:21 +0100

    chore: include auth provider to root layout :fire:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
 import "./global.css"
+import "../config/google-config";
+import { AuthProvider } from "@/context/AuthContext";
+
+
 export default function RootLayout() {
-  return <Stack />;
+
+
+  return (
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </AuthProvider>
+  )
 }
