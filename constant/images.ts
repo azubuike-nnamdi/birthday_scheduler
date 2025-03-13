239c4850d94cc05a17f4d9484e2d135a20c2ac4b
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-14 00:37:17 +0100

    chore: add new iimages :zap:

--- constant/images.ts
+++ constant/images.ts
@@ -1,9 +1,17 @@
 import LadyWithCake1 from "@/assets/images/lady-with-cake1.png";
 import LadyWithCake2 from "@/assets/images/lady-with-cake2.png";
 import LadyWithCake2Svg from "@/assets/images/lady-with-cake2.svg";
+import LadyWithPhone from "@/assets/images/lady-with-phone.png";
+import SmilingLady from "@/assets/images/smiling-lady.png";
+import LadyShopping from "@/assets/images/lady-shopping.png";
+import LadyHavingCoffee from "@/assets/images/lady-taking-coffee.png";
 
 export default {
   LadyWithCake1,
   LadyWithCake2,
   LadyWithCake2Svg,
+  LadyWithPhone,
+  SmilingLady,
+  LadyShopping,
+  LadyHavingCoffee,
 };
