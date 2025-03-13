73d1e9d17ed872775a5ce0019cab169c34c87422
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-13 10:55:53 +0100

    feat: export of icons and image configured :racehorse:

---
+++ constant/images.ts
@@ -0,0 +1,9 @@
+import LadyWithCake1 from "@/assets/images/lady-with-cake1.png";
+import LadyWithCake2 from "@/assets/images/lady-with-cake2.png";
+import LadyWithCake2Svg from "@/assets/images/lady-with-cake2.svg";
+
+export default {
+  LadyWithCake1,
+  LadyWithCake2,
+  LadyWithCake2Svg,
+};
